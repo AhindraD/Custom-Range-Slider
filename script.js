1cb9367af83c39e6c71a2500fec1bba2f8e866b0
Ahindra D <official.ahindra@gmail.com>
2022-09-01 09:55:28 +0530

    done w/o drag

---
+++ script.js
@@ -0,0 +1,13 @@
+const targetSlider = document.querySelector(".slider");
+
+targetSlider.addEventListener("mousemove", (e) => {
+    let xPos = e.clientX - targetSlider.offsetLeft;
+
+    //console.log(xPos);
+    xPos = xPos < 0 ? 0 : xPos;
+    xPos = xPos > 300 ? 300 : xPos;
+    document.documentElement.style.setProperty('--fill', `${(xPos)}px`);
+    document.documentElement.style.setProperty('--slide', `${(xPos - 10)}px`);
+
+    document.querySelector(".sub-val").innerHTML = parseInt(xPos / 3);
+})
